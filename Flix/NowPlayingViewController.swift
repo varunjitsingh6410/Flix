2eadcd119fce60d8f463f2f27ea2faf04e4d3902
Varun Jit Singh <varun@administorsmbp2.attlocal.net>
2018-12-13 13:24:28 -0800

    Added pull to refresh functionality

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -24,6 +24,9 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         movieFeed.dataSource = self
         movieFeed.delegate = self
         
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControl.Event.valueChanged)
+        movieFeed.insertSubview(refreshControl, at: 0)
         
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -79,6 +82,39 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
         return 200.0//Choose your custom row height
+    }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        // Do any additional setup after refreshing the view.
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+                
+                // TODO: Get the array of movies
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print (title)
+                }
+                // TODO: Store the movies in a property to use elsewhere
+                self.movies = movies
+                
+                // TODO: Reload your table view data
+                self.movieFeed.reloadData()
+            }
+        }
+        refreshControl.endRefreshing()
+        task.resume()
     }
     
     
